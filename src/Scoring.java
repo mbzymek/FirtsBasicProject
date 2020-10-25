159bbaef382c076693d73054852134d8489c8121
Bzymek Mateusz <mbzymek99@wp.pl>
2020-10-25 22:59:12 +0100

    Poprawki, dodanie ostatecznej logiki do EASY brakuje logiki dla MEDIUM/HARD plus trzeba uporządkować kod :)

--- src/Scoring.java
+++ src/Scoring.java
@@ -28,22 +28,37 @@ public class Scoring {
         //int yourChoice = chooseNumber();
         //System.out.println("Your choice is: "+ yourChoice);
         int los;
+        int licznik = 0;
         consoleReader.readInputParameters();
         switch (levelOfDifficulties.getLevelOfDifficultiesType()){
+
             case EASY:
                 los = calculateEasy.calculate();
                 System.out.println("los to "+los);
-                System.out.println("Wprowadz liczbe od 1 do 10");
-                i = in.nextInt();
-                if(i < calculateEasy.calculate()){
-                    System.out.println("Your number is too small");
+
+                int k =1;
+                while(k <= 3){
+                    System.out.println("Wprowadz liczbe od 1 do 10");
+                    i = in.nextInt();
+                        if(i < los){
+                            System.out.println("Your number is too small");
+                        }
+                        else if(i > los){
+                            System.out.println("Your number is too big");
+                        }
+                        else if(i == los){
+                            System.out.println("Congratulations you WIN!!!");
+                        }
+                        licznik ++;
+                        if(licznik == 3 && i != los)
+                        {
+                            System.out.println("Przegrales, wykorzystales 3 próby");
+                        }
+
+
+                    k++;
                 }
-                else if(i > calculateEasy.calculate()){
-                    System.out.println("Your number is too big");
-                }
-                else{
-                    System.out.println("Congratulations you WIN!!!");
-                }
+
                 break;
             case MEDIUM:
                 los = calculateMedium.calculate();
